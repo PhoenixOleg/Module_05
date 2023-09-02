fea7fa332a3752b0072d1575bf3e433e454954f6
PhoenixOleg <metall_2000@mail.ru>
2023-09-02 23:37:43 +0300

    Рекурсия. Подготовка к итоговому заданию.

--- Module_05/Program.cs
+++ Module_05/Program.cs
@@ -6,36 +6,7 @@ namespace Module_05
     {
         static void Main(string[] args)
         {
-            //Юнит - Рекурсия
-            //Задание 5.5.3
-
-            Console.WriteLine("Напишите что-то");
-            var str = Console.ReadLine();
-
-            Console.WriteLine("Укажите глубину эха");
-            var deep = int.Parse(Console.ReadLine());
-
-            Echo(str, deep);
-
-            Console.ReadKey();
-        }
-
-        static void Echo(string saidworld, int deep)
-        {
-            var modif = saidworld;
-            
-            if (modif.Length > 2)
-            {
-                modif = modif.Remove(0, 2);
-            }
-
-            Console.WriteLine("..." + modif);
-
-            if (deep > 1)
-            {
-                Echo(modif, deep - 1);
-            }
-
+            //Итоговое задание модуля 5.
         }
     }
 }
---
+++ Module_05/Recursion.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_05
+{
+    class Recursion
+    {
+        static void Tasks_Ubit_5()
+        {
+            //Юнит - Рекурсия
+            //Задание 5.5.3
+
+            //Console.WriteLine("Напишите что-то");
+            //var str = Console.ReadLine();
+
+            //Console.WriteLine("Укажите глубину эха");
+            //var deep = int.Parse(Console.ReadLine());
+
+            //Echo(str, deep);
+
+            ////Задание 5.5.5
+            //Console.WriteLine(Factorial(20));
+
+            //Задание 5.5.8
+            Console.WriteLine(PowerUp(3, 5));
+            
+            Console.ReadKey();
+        }
+
+        static void Echo(string saidworld, int deep)
+        {
+            var modif = saidworld;
+
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+            }
+
+            Console.BackgroundColor = (ConsoleColor)deep; //Приведение int в System.ConsoleColor
+            Console.WriteLine("..." + modif);
+
+            if (deep > 1)
+            {
+                Echo(modif, deep - 1);
+            }
+        }
+
+        static decimal Factorial(int x) //Задание 5.5.5
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+
+        private static int PowerUp(int N, byte pow) //Задание 5.5.8
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                if (pow == 1)
+                {
+                    return N;
+                }
+                else
+                {
+                    return N * PowerUp(N, --pow);
+                }
+            }
+        }
+    }
+}
